acb95e3d7005a2bb49817de8ca5e026e3aebdc13
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-08 15:28:35 +0900

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by 東郷翔士希 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
